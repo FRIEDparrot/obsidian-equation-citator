db25f718e550f1d43e86e973cd9753e682484807
FRIEDparrot <814680344@qq.com>
2026-06-21 22:57:37 +0800

    added global types to the obsidian.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,6 +1,11 @@
 import "obsidian";
 import type { EditorView } from "@codemirror/view";
 
+declare global {
+    const activeWindow: Window;
+    const activeDocument: Document;
+}
+
 declare module "obsidian" {
     interface Editor {
         cm: EditorView;
