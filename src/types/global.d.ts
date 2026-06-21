960b4d8ff4c3b1f7b8448354e0e7d0016464f6e5
FRIEDparrot <814680344@qq.com>
2026-06-21 23:12:16 +0800

    add type declaration in the   eslint.config.mjs

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,11 +1,6 @@
 import "obsidian";
 import type { EditorView } from "@codemirror/view";
 
-declare global {
-    const activeWindow: Window;
-    const activeDocument: Document;
-}
-
 declare module "obsidian" {
     interface Editor {
         cm: EditorView;
